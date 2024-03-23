61a13f1e9e339ac7f9d2335985d94d15b31de826
KarVarr <megatr9n@gmail.com>
2024-03-24 00:25:37 +0400

    added the formula

--- Work/excelHM.py
+++ Work/excelHM.py
@@ -7,25 +7,23 @@ fn = "HM.xlsx"
 book = openpyxl.open(fn)
 sheet = book.active
 
-capitals = ['IDs','Title','Color','Material','Description','Concept','Photos']
+capitals = ['IDs','Title','Color','Material','Description','Concept','Photos','', 'Formula' ]
 sheet.insert_cols(1, amount=len(capitals))
 
 
 
 for i in range(1, sheet.max_row + 1):
-    value = sheet.cell(row=i, column=9).value
+    value = sheet.cell(row=i, column=11).value
     sheet.cell(row=i, column=1).value = value
 
 # Copy data from column I to a list
-# for i in range(2, sheet.max_row + 1):
-#     column_I_data = sheet.cell(row=i, column=9).value
-    column_I_data = [sheet.cell(row=i, column=9).value for i in range(2, sheet.max_row + 1)]
+column_I_data = [sheet.cell(row=i, column=11).value for i in range(2, sheet.max_row + 1)]
 
-    # Sort the data in ascending order
-    column_I_data.sort()
+# Sort the data in ascending order
+column_I_data.sort()
 
-    # Remove duplicates
-    column_I_data = list(dict.fromkeys(column_I_data))
+# Remove duplicates
+column_I_data = list(dict.fromkeys(column_I_data))
 
 # Clear existing data in column A
 for i in range(1, sheet.max_row + 1):
@@ -34,6 +32,14 @@ for i in range(1, sheet.max_row + 1):
 # Write sorted and deduplicated data to column A
 for i, value in enumerate(column_I_data, start=1):
     sheet.cell(row=i, column=1).value = value
+
+# Write formula into the second column
+formula = "=VLOOKUP(A2, K:T, 2, FALSE)"
+sheet.cell(row=2, column=2).value = formula
+
+# Fill the formula down to the last row of data in column A
+for i in range(3, len(column_I_data) + 1):  # Start from row 3 since the formula is already in row 2
+    sheet.cell(row=i, column=2).value = "=VLOOKUP(A{}, K:T, 2, FALSE)".format(i)
 
 
 for i, capital in enumerate(capitals, start=1):
