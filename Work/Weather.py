138ef54a29845630c59fc604999feefdf7e0a42b
KarVarr <megatr9n@gmail.com>
2024-03-24 22:05:15 +0400

    added code into the Weather file for get weather in the city

--- Work/Weather.py
+++ Work/Weather.py
@@ -1,2 +1,22 @@
-import requests
+import sys
+sys.path.append('../Work/api')
 
+import requests
+from api import my_api
+
+api_key = my_api.api_open_weather_map
+city = 'Yerevan'
+lang = 'ru'
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang={lang}&units=metric'
+
+response = requests.get(url)
+data = response.json()
+
+weather_description = data['weather'][0]['description']
+temperature = data['main']['temp']
+humidity = data['main']['humidity']
+
+print(f'Today in {city}')
+print(f'Descriptino: {weather_description.capitalize()}')
+print(f'Temperature: {temperature}')
+print(f'Humidity: {humidity}')
