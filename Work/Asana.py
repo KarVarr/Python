7df1458f63a89b4a8e56233994b339653344f1f9
KarVarr <megatr9n@gmail.com>
2024-03-27 22:15:33 +0400

    added 100daysofcode folder

--- Work/Asana.py
+++ Work/Asana.py
@@ -13,10 +13,17 @@ users_api_instance = asana.UsersApi(api_client)
 user_gid = "me"
 opts = {}
 
-try:
-    me = users_api_instance.get_user(user_gid, opts)
+tasks_api_instance = asana.TasksApi(api_client)
 
-    print('Hello world, its me: ' + me['name'] + "!" )
+try:
+    # me = users_api_instance.get_user(user_gid, opts)
+    # print('Hello world, its me: ' + me['name'] + "!" )
+    
+    tasks = tasks_api_instance.get_tasks(opts)
+    print("List taksk:")
+    for task in tasks:
+        print(task)
+    
 
 except ApiException as e:
     print("Exception when calling UsersApi->get_user: %s\n" % e)
